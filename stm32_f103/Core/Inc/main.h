357ed6e41d11e86301471830f75fc368075a962a
Kamila Neliba <43151316+kneliba@users.noreply.github.com>
2022-02-25 15:48:56 -0800

    motor encoder file init

--- stm32_f103/Core/Inc/main.h
+++ stm32_f103/Core/Inc/main.h
@@ -29,6 +29,7 @@ extern "C" {
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f1xx_hal.h"
+#include "motor_encoder.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
