40e20fda0634761779778f1437331330d824c89f
j-une <50782275+j-une@users.noreply.github.com>
2022-02-27 01:33:19 -0500

    created read magnetometer function

--- stm32_f103/Core/Inc/imu.h
+++ stm32_f103/Core/Inc/imu.h
@@ -1,2 +1,25 @@
-// Function Prototypes
+// Read gyroscope
+void ICM20948_Read_Gyro();
+
+// Read accelerometer
+void ICM20948_Read_Accel();
+
+// Read magnetometer
 void ICM20948_Read_Magn();
+
+// Axis typedef
+typedef struct
+{
+	float x;
+	float y;
+	float z;
+} axises;
+
+// Define read write addresses
+#define READ							0x80
+#define WRITE							0x00
+
+// Define magnetometer registers
+#define AK09916_ADDRESS 				0x0C
+#define MAG_HXL							0x11
+
--- stm32_f103/Core/Src/imu.c
+++ stm32_f103/Core/Src/imu.c
@@ -1,5 +1,39 @@
+#include "imu.h"
+#include "stm32f1xx_hal.h"
+#include "stm32f1xx_hal_tim.h"
+#include "stm32f1xx_hal_gpio.h"
+#include "stm32f103xb.h"
+#include "main.h"
+
+// void ICM20948_Read_Gyro();
+
+// void ICM20948_Read_Gyro();
+
 void ICM20948_Read_Magn()
 {
-    // HAL_I2C_Master_Transmit()
-    // HAL_I2C_Master_Receive()
+	HAL_StatusTypeDef ret;
+	axises* data;
+	uint8_t temp[6];
+
+	// send write request to AK09916
+    ret = HAL_I2C_Master_Transmit(&hi2c1, AK09916_ADDR << 1, &MAG_HXL, 1, 1000);
+
+    if(ret != HAL_OK)
+    {
+    	return;
+    }
+
+	HAL_Delay(1);
+
+	// read data starting from lower x-axis
+	HAL_I2C_Master_Receive(&hi2c1, AK09916_ADDR << 1, temp, 6, 1000);
+
+	// separate data into axises
+    data->x = (int16_t)(temp[1] << 8 | temp[0]);
+	data->y = (int16_t)(temp[3] << 8 | temp[2]);
+	data->z = (int16_t)(temp[5] << 8 | temp[4]);
+
+	// do something with data, convert to heading
 }
+
+// need function to select user bank ?
