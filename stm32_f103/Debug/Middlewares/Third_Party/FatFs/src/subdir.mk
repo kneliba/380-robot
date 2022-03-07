4e4b3d4c51b367f786fccb9b26de84b224b5b800
Kamila Neliba <43151316+kneliba@users.noreply.github.com>
2022-03-07 14:13:14 -0500

    compile

--- stm32_f103/Debug/Middlewares/Third_Party/FatFs/src/subdir.mk
+++ stm32_f103/Debug/Middlewares/Third_Party/FatFs/src/subdir.mk
@@ -5,29 +5,44 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
+../Middlewares/Third_Party/FatFs/src/diskio\ 3.c \
 ../Middlewares/Third_Party/FatFs/src/diskio.c \
+../Middlewares/Third_Party/FatFs/src/ff\ 3.c \
 ../Middlewares/Third_Party/FatFs/src/ff.c \
+../Middlewares/Third_Party/FatFs/src/ff_gen_drv\ 3.c \
 ../Middlewares/Third_Party/FatFs/src/ff_gen_drv.c 
 
 OBJS += \
+./Middlewares/Third_Party/FatFs/src/diskio\ 3.o \
 ./Middlewares/Third_Party/FatFs/src/diskio.o \
+./Middlewares/Third_Party/FatFs/src/ff\ 3.o \
 ./Middlewares/Third_Party/FatFs/src/ff.o \
+./Middlewares/Third_Party/FatFs/src/ff_gen_drv\ 3.o \
 ./Middlewares/Third_Party/FatFs/src/ff_gen_drv.o 
 
 C_DEPS += \
+./Middlewares/Third_Party/FatFs/src/diskio\ 3.d \
 ./Middlewares/Third_Party/FatFs/src/diskio.d \
+./Middlewares/Third_Party/FatFs/src/ff\ 3.d \
 ./Middlewares/Third_Party/FatFs/src/ff.d \
+./Middlewares/Third_Party/FatFs/src/ff_gen_drv\ 3.d \
 ./Middlewares/Third_Party/FatFs/src/ff_gen_drv.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
+Middlewares/Third_Party/FatFs/src/diskio\ 3.o: ../Middlewares/Third_Party/FatFs/src/diskio\ 3.c Middlewares/Third_Party/FatFs/src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../Core/Inc -I../FATFS/Target -I../FATFS/App -I../Middlewares/Third_Party/FatFs/src -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/STM32F1xx_HAL_Driver/Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Middlewares/Third_Party/FatFs/src/diskio 3.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 Middlewares/Third_Party/FatFs/src/%.o: ../Middlewares/Third_Party/FatFs/src/%.c Middlewares/Third_Party/FatFs/src/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../Core/Inc -I../FATFS/Target -I../FATFS/App -I../Middlewares/Third_Party/FatFs/src -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/STM32F1xx_HAL_Driver/Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Middlewares/Third_Party/FatFs/src/ff\ 3.o: ../Middlewares/Third_Party/FatFs/src/ff\ 3.c Middlewares/Third_Party/FatFs/src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../Core/Inc -I../FATFS/Target -I../FATFS/App -I../Middlewares/Third_Party/FatFs/src -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/STM32F1xx_HAL_Driver/Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Middlewares/Third_Party/FatFs/src/ff 3.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Middlewares/Third_Party/FatFs/src/ff_gen_drv\ 3.o: ../Middlewares/Third_Party/FatFs/src/ff_gen_drv\ 3.c Middlewares/Third_Party/FatFs/src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../Core/Inc -I../FATFS/Target -I../FATFS/App -I../Middlewares/Third_Party/FatFs/src -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/STM32F1xx_HAL_Driver/Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Middlewares/Third_Party/FatFs/src/ff_gen_drv 3.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 
 clean: clean-Middlewares-2f-Third_Party-2f-FatFs-2f-src
 
 clean-Middlewares-2f-Third_Party-2f-FatFs-2f-src:
-	-$(RM) ./Middlewares/Third_Party/FatFs/src/diskio.d ./Middlewares/Third_Party/FatFs/src/diskio.o ./Middlewares/Third_Party/FatFs/src/ff.d ./Middlewares/Third_Party/FatFs/src/ff.o ./Middlewares/Third_Party/FatFs/src/ff_gen_drv.d ./Middlewares/Third_Party/FatFs/src/ff_gen_drv.o
+	-$(RM) ./Middlewares/Third_Party/FatFs/src/diskio\ 3.d ./Middlewares/Third_Party/FatFs/src/diskio\ 3.o ./Middlewares/Third_Party/FatFs/src/diskio.d ./Middlewares/Third_Party/FatFs/src/diskio.o ./Middlewares/Third_Party/FatFs/src/ff\ 3.d ./Middlewares/Third_Party/FatFs/src/ff\ 3.o ./Middlewares/Third_Party/FatFs/src/ff.d ./Middlewares/Third_Party/FatFs/src/ff.o ./Middlewares/Third_Party/FatFs/src/ff_gen_drv\ 3.d ./Middlewares/Third_Party/FatFs/src/ff_gen_drv\ 3.o ./Middlewares/Third_Party/FatFs/src/ff_gen_drv.d ./Middlewares/Third_Party/FatFs/src/ff_gen_drv.o
 
 .PHONY: clean-Middlewares-2f-Third_Party-2f-FatFs-2f-src
 
