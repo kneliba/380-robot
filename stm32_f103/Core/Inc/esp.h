811f3071ecd30003ea683f1c4a7b953734e56e4a
j-une <50782275+j-une@users.noreply.github.com>
2022-03-14 11:59:36 -0400

    stm32 as slave

--- stm32_f103/Core/Inc/esp.h
+++ stm32_f103/Core/Inc/esp.h
@@ -3,6 +3,6 @@
 #include "main.h"
 
 void ESP_send(I2C_HandleTypeDef *hi2c1, uint8_t register_address, uint16_t size, uint8_t data);
-void ESP_receive(I2C_HandleTypeDef *hi2c1, uint8_t register_address, uint16_t size, uint8_t *Receive);
+void ESP_receive(I2C_HandleTypeDef *hi2c1, uint16_t size, uint8_t *Receive);
 
 #endif /* ESP_H */
--- stm32_f103/Core/Src/esp.c
+++ stm32_f103/Core/Src/esp.c
@@ -6,16 +6,13 @@
 #include "stm32f103xb.h"
 #include "main.h"
 
-uint8_t ESP_ADDRESS = 0x08; // not sure if correct, may have to check for address first
-
 void ESP_send(I2C_HandleTypeDef *hi2c1, uint8_t register_address, uint16_t size, uint8_t data)
 {
 	 uint8_t Trans[2]={register_address, data};
-	 HAL_I2C_Master_Transmit(hi2c1,ESP_ADDRESS << 1,Trans,size,1000);
+	 HAL_I2C_Slave_Transmit(hi2c1,Trans,size,1000);
 }
 
-void ESP_receive(I2C_HandleTypeDef *hi2c1, uint8_t register_address, uint16_t size, uint8_t *Receive)
+void ESP_receive(I2C_HandleTypeDef *hi2c1, uint16_t size, uint8_t *Receive)
 {
-	HAL_I2C_Master_Transmit(hi2c1,ESP_ADDRESS << 1,&register_address,1,1000);
-	HAL_I2C_Master_Receive(hi2c1,ESP_ADDRESS << 1,Receive,size,1000);
+	HAL_I2C_Slave_Receive(hi2c1,Receive,size,1000);
 }
