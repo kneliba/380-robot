9a601cd409de86f311ca0057951b0fc33e2dfc43
Dawson Kletke <dawsonwkletke@gmail.com>
2022-03-14 13:21:15 -0400

    Update STM32 slave address for I2C with ESP (I2C1)

--- stm32_f103/Core/Src/main.c
+++ stm32_f103/Core/Src/main.c
@@ -229,7 +229,7 @@ static void MX_I2C1_Init(void)
   hi2c1.Instance = I2C1;
   hi2c1.Init.ClockSpeed = 100000;
   hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
-  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.OwnAddress1 = 16;
   hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
   hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
   hi2c1.Init.OwnAddress2 = 0;
