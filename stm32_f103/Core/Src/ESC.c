eaff5a1f837913b1e3c02f882295eb8df7bfebd4
j-une <50782275+j-une@users.noreply.github.com>
2022-03-29 19:08:59 -0400

    start combining error correction and run course

--- stm32_f103/Core/Src/ESC.c
+++ stm32_f103/Core/Src/ESC.c
@@ -63,6 +63,55 @@ void decelerate (TIM_HandleTypeDef *htim)
 	}
 }
 
+// increases one side of the motor to drive faster to offset a leer
+// can reduce or increase the tolerance from the ideal path and correction speed
+// can also reduce the speed of the other motor to stay on path
+void drive_straight_ultrasonic (TIM_HandleTypeDef *htim, double speed, double ideal_block_distance)
+{
+
+	double pulse_width = 1.0 + (speed/100.0);
+	double command = (pulse_width/20.0)*ARR;
+
+	uint8_t correction_speed = 5;
+	double pulse_width_correction = 1.0 + ((speed+ correction_speed)/100.0);
+	double increased_command = (pulse_width_correction/20.0)*ARR;
+
+
+	double distance_from_wall = get_side_distance();
+	int tolerance = 3;
+
+	// to the right of the ideal path
+	if(distance_from_wall > ideal_block_distance + tolerance) {
+		// Correct by driving to the left
+		TIM2->CCR1 = command; // left
+		TIM2->CCR2 = increased_command; // right
+	}
+	// to the left of the ideal path
+	else if(distance_from_wall < ideal_block_distance - tolerance) {
+		// Correct by driving to the right
+		TIM2->CCR1 = increased_command; // right
+		TIM2->CCR2 = command; // right
+	}
+	// on ideal path
+	else{
+		// drive straight
+		TIM2->CCR1 = command; // left
+		TIM2->CCR2 = command; // right
+	}
+}
+
+void drive_straight_distance_ultrasonic (TIM_HandleTypeDef *htim, double speed, double distance, double ideal_block_distance)
+{
+	reset_distance();
+	int16_t  encoder_dist = get_distance_travelled();
+	drive_straight_ultrasonic(htim, speed, ideal_block_distance);
+	while (encoder_dist < distance) {
+//		if(encoder_dist > )encoder_dists
+		drive_straight_ultrasonic(htim, speed, ideal_block_distance);
+	}
+	stop(htim);
+}
+
 // Integration functions
 // drive a set distance (with encoder)
 void drive_distance (TIM_HandleTypeDef *htim, double speed, double distance)
@@ -82,4 +131,3 @@ void drive_until (TIM_HandleTypeDef *htim, double speed, double distance)
 	while (ultrasonic_dist < distance) {}
 	stop(htim);
 }
-
--- stm32_f103/Core/Src/run_course.c
+++ stm32_f103/Core/Src/run_course.c
@@ -15,11 +15,14 @@
 // TODO: test how much distance is required to turn
 static double dist_to_turn = 15;
 static double dist_of_tile = 30.5;
+static double dist_beside_wall = 10;
 
 // number of tiles to wall
 double distances_to_wall[11] = {0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2};
 // number of tiles to travel
 double distances_to_travel[11] = {4, 5, 5, 4, 4, 3, 3, 2, 2, 1, 1};
+// distance to side wall
+double distances_to_side[11] = {0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2};
 
 // keep track of where robot is on course
 int8_t course[6][6] = {{0, 0, 0, 0, 0, 0},
@@ -29,6 +32,9 @@ int8_t course[6][6] = {{0, 0, 0, 0, 0, 0},
 					   {0, 0, 0, 0, 0, 0},
 					   {0, 0, 0, 0, 1, 0}
 					   };
+int8_t current_pos[2] = {5,4};
+int8_t full_path[2] = {{5,4}, {5,3}, {5,2}}; // finish this
+
 int8_t pits[3][2] = {{2, 5,},
 					 {3, 5,},
 					 {4, 4,}
@@ -78,7 +84,7 @@ void run_course_encoders (TIM_HandleTypeDef *htim, double speed)
 	while(current < 11)
 	{
 		// error correction, use angle or side ultrasonic to fix angle
-		drive_distance(htim, speed, distances_to_wall[current]);
+		drive_distance(htim, speed, distances_to_travel[current]);
 		turn_right(htim);
 		current++;
 	}
@@ -87,5 +93,21 @@ void run_course_encoders (TIM_HandleTypeDef *htim, double speed)
 // run full course by distance using motor encoders and ultrasonic
 void run_course_encoders_ultrasonic (TIM_HandleTypeDef *htim, double speed)
 {
-	// use motor encoders unless in pit
+	// calculate distance to travel in cm
+	for(int8_t i = 0; i < 11; i++)
+	{
+		distances_to_travel[i] *= dist_of_tile;
+		distances_to_side[i] *= dist_of_tile;
+		distances_to_side[i] += dist_beside_wall;
+	}
+
+	int8_t current = 0;
+
+	while(current < 11)
+	{
+
+		drive_straight_distance_ultrasonic(htim, speed, distances_to_travel[current], distances_to_side[current]);
+		turn_right(htim);
+		current++;
+	}
 }
