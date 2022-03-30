4f6079b8a9880ebb11102bef03549bea1cc64b95
Kamila Neliba <43151316+kneliba@users.noreply.github.com>
2022-03-29 22:47:06 -0400

    updating drive straight ultrasonic

--- stm32_f103/Core/Src/ESC.c
+++ stm32_f103/Core/Src/ESC.c
@@ -69,34 +69,39 @@ void decelerate (TIM_HandleTypeDef *htim)
 void drive_straight_ultrasonic (TIM_HandleTypeDef *htim, double speed, double ideal_block_distance)
 {
 
-	double pulse_width = 1.0 + (speed/100.0);
-	double command = (pulse_width/20.0)*ARR;
+	double pulse_widthL = 1.0 + (speed*1.25/100.0);
+	double commandL = (pulse_widthL/20.0)*ARR;
 
-	uint8_t correction_speed = 5;
-	double pulse_width_correction = 1.0 + ((speed+ correction_speed)/100.0);
-	double increased_command = (pulse_width_correction/20.0)*ARR;
-
+	double pulse_widthR = 1.0 + (speed/100.0);
+	double commandR = (pulse_widthR/20.0)*ARR;
 
 	double distance_from_wall = get_side_distance();
 	int tolerance = 3;
 
+	//float error = distance_from_wall - ideal_block_distance;
+
+	uint8_t correction_speed = 15;
+	double pulse_width_correction = 1.0 + ((correction_speed)/100.0);
+	double increased_command = (pulse_width_correction/20.0)*ARR;
+
+
 	// to the right of the ideal path
 	if(distance_from_wall > ideal_block_distance + tolerance) {
 		// Correct by driving to the left
-		TIM2->CCR1 = command; // left
-		TIM2->CCR2 = increased_command; // right
+		TIM2->CCR1 = commandL; // left
+		TIM2->CCR2 = commandR + increased_command; // right
 	}
 	// to the left of the ideal path
 	else if(distance_from_wall < ideal_block_distance - tolerance) {
 		// Correct by driving to the right
-		TIM2->CCR1 = increased_command; // right
-		TIM2->CCR2 = command; // right
+		TIM2->CCR1 = commandL+ increased_command; // right
+		TIM2->CCR2 = commandR; // right
 	}
 	// on ideal path
 	else{
 		// drive straight
-		TIM2->CCR1 = command; // left
-		TIM2->CCR2 = command; // right
+		TIM2->CCR1 = commandL; // left
+		TIM2->CCR2 = commandR; // right
 	}
 }
 
