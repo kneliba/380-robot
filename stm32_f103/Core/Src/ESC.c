c4d5c62ad9c018b499df0b30a6cd76b4facca994
Kamila Neliba <43151316+kneliba@users.noreply.github.com>
2022-03-30 16:38:31 -0400

    side ultrasonic error to imu angle

--- stm32_f103/Core/Src/ESC.c
+++ stm32_f103/Core/Src/ESC.c
@@ -205,7 +205,7 @@ void decelerate(TIM_HandleTypeDef *htim)
 
 // increases one side of the motor to drive faster to offset a leer
 // can also reduce the speed of the other motor to stay on path
-void drive_straight_ultrasonic(TIM_HandleTypeDef *htim, double speed, double ideal_block_distance, int tolerance)
+void drive_straight_ultrasonic_P(TIM_HandleTypeDef *htim, double speed, double ideal_block_distance)
 {
 	uint8_t kp = 8;
 	double pulse_widthL = 1.0 + (speed * L_offset / 100.0);
@@ -257,6 +257,25 @@ void drive_straight_ultrasonic(TIM_HandleTypeDef *htim, double speed, double ide
 		TIM2->CCR1 = commandL; // left
 		TIM2->CCR2 = commandR; // right
 	}
+}
+
+// increases one side of the motor to drive faster to offset a leer
+// can also reduce the speed of the other motor to stay on path
+void drive_straight_ultrasonic_IMU(TIM_HandleTypeDef *htim, I2C_HandleTypeDef *hi2c2, double speed, double ideal_block_distance, double current_angle)
+{
+	uint8_t kp = 0.8;
+	double pulse_widthL = 1.0 + (speed * L_offset / 100.0);
+	double pulse_widthR = 1.0 + (speed / 100.0);
+
+	uint16_t commandL = (pulse_widthL / 20.0) * ARR;
+	uint16_t commandR = (pulse_widthR / 20.0) * ARR;
+
+	double distance_from_wall = get_side_distance();
+
+	float distance_error = distance_from_wall - ideal_block_distance;
+	float desired_angle = distance_error*kp;
+
+	drive_straight(htim , speed, hi2c2, desired_angle, current_angle);
 }
 
 void drive_straight_distance_ultrasonic(TIM_HandleTypeDef *htim, double speed, double distance, double ideal_block_distance)
